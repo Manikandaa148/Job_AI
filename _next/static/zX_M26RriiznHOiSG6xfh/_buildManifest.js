96817662c9cf70e62634bd06389bdacc9a7efdf5
Manikandaa148 <manikandaa944@gmail.com>
2025-12-16 09:45:03 +0530

    Manual deployment to fix 404

---
+++ _next/static/zX_M26RriiznHOiSG6xfh/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Job_AI//_next/:path+",
+        "destination": "/Job_AI/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
